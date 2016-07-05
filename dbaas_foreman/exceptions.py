9a74fbb5af786475ec00efde98166368832705e4
felippemr <raposo.felippe@gmail.com>
2016-07-05 12:45:35 -0300

    Add exceptions

---
+++ dbaas_foreman/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class HostNotFoundException(Exception):
+    pass
+
+
+class HostParameterNotCreatedError(Exception):
+    pass
+
+
+class HostPuppetClassNotCreatedError(Exception):
+    pass
+
+
+class PuppetClassNotFound(Exception):
+    pass
