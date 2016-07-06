4f120b8c2d643206eb8b4a7e1dba2fcb5c0d94d7
otherpirate <mauro_murari@hotmail.com>
2016-07-06 16:22:51 -0300

    New release 0.1.3

--- dbaas_foreman/__init__.py
+++ dbaas_foreman/__init__.py
@@ -5,7 +5,7 @@ from dbaas_foreman.foreman_provider import ForemanProvider
 
 __author__ = 'Felippe da Motta Raposo'
 __email__ = 'raposo.felippe@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 
 def get_foreman_provider(databaseinfra, credentials):
